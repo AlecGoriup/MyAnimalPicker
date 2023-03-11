9ee4695f045d52fd9d6b337f589eb380f0be112f
AlecGoriup <113146110+AlecGoriup@users.noreply.github.com>
2023-03-10 20:08:35 -0500

    user can choose dog or cat

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,6 +4,20 @@ public class AnimalPicker
 {
     public static void main(String[] args)
     {
-        
+        Scanner scan = new Scanner(System.in);
+        String animal;
+
+        System.out.println("What animal would you like to see: dog or cat");
+        animal = scan.nextLine();
+
+        if (animal.equals("dog"))
+        {
+            System.out.println("DOG ASCII ART");
+        }
+
+        else if (!animal.equals("cat"))
+        {
+            System.out.println("CAT ASCII ART");
+        }
     }
 }
